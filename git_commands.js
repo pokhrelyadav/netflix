23c0f3a5e38349138b9f4f94c317183fb7022c04
pokhrelyadav <pokhrelyadav412@gmail.com>
2024-05-30 07:30:16 +0545

    Updated git command file

--- git_commands.js
+++ git_commands.js
@@ -6,3 +6,10 @@
 //  to add remote => git remote add <name> <link>
 // git branch -M main
 //  git push -u <name> <branch>
+
+//git branch -m master main - rename master to main . -m means move i.e it renames
+// git remote set-url  origin https://github.com/pokhrelyadav/amazon.git    to add our github repo remote link to cloned projects
+
+//? To change recent commit message
+// git commit --amend      - opens text editor , edit and press Esc to enter command mode and write  :wq or :q! and enter then,
+// git push --force
